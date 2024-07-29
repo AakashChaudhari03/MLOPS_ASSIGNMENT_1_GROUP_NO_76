fd7147da04197eb578e4383c1b091b9eeae4580b
AakashChaudhari03 <2022ac05607@wilp.bits-pilani.ac.in>
2024-07-29 22:42:50 +0530

    W293 blank line contains whitespace removed

--- src/train.py
+++ src/train.py
@@ -10,20 +10,20 @@ def train_model():
     # Load dataset
     iris = load_iris()
     X, y = iris.data, iris.target
-    
+
     # Split the dataset
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42
     )
-    
+
     # Train model
     model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
-    
+
     # Save the model
     with open('models/iris_model.pkl', 'wb') as model_file:
         pickle.dump(model, model_file)
-    
+
     return model
 
 
